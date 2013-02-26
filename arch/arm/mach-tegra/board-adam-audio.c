3fa73b8068ffbecee1553881e0246e510b2f79ca
Boris Popov <bvpopov@gmail.com>
2013-02-26 11:08:13 -0800

    board: adam: Audio clean up

--- arch/arm/mach-tegra/board-adam-audio.c
+++ arch/arm/mach-tegra/board-adam-audio.c
@@ -61,148 +61,6 @@
 	Codec MCLK = APxx DAP_MCLK1
 */
 
-#ifdef USE_ORG_DAS
-
-#include <mach/tegra_das.h>
-
-static struct tegra_das_platform_data tegra_das_pdata = {
-	.dap_clk = "clk_dev1",
-	.tegra_dap_port_info_table = {
-		/* I2S1 <--> DAC1 <--> DAP1 <--> Hifi Codec */
-		[0] = {
-			.dac_port = tegra_das_port_i2s1,
-			.dap_port = tegra_das_port_dap1,
-			.codec_type = tegra_audio_codec_type_hifi,
-			.device_property = {
-				.num_channels = 2,
-				.bits_per_sample = 16,
-#ifdef ADAM_48KHZ_AUDIO				
-				.rate = 48000,
-#else
-				.rate = 44100,
-#endif
-				.dac_dap_data_comm_format =
-						dac_dap_data_format_all,
-			},
-		},
-		/* I2S2 <--> DAC2 <--> DAP2 <--> Voice Codec */
-		[1] = {
-			.dac_port = tegra_das_port_i2s2,
-			.dap_port = tegra_das_port_dap2,
-			.codec_type = tegra_audio_codec_type_voice,
-			.device_property = {
-				.num_channels = 1,
-				.bits_per_sample = 16,
-				.rate = 8000,
-				.dac_dap_data_comm_format =
-						dac_dap_data_format_all,
-			},
-		},
-		/* I2S2 <--> DAC2 <--> DAP3 <--> Baseband Codec */
-		[2] = {
-			.dac_port = tegra_das_port_i2s2,
-			.dap_port = tegra_das_port_dap3,
-			.codec_type = tegra_audio_codec_type_baseband,
-			.device_property = {
-				.num_channels = 1,
-				.bits_per_sample = 16,
-				.rate = 8000,
-				.dac_dap_data_comm_format =
-					dac_dap_data_format_dsp,
-			},
-		},
-		/* I2S2 <--> DAC2 <--> DAP4 <--> BT SCO Codec */
-		[3] = {
-			.dac_port = tegra_das_port_i2s2,
-			.dap_port = tegra_das_port_dap4,
-			.codec_type = tegra_audio_codec_type_bluetooth,
-			.device_property = {
-				.num_channels = 1,
-				.bits_per_sample = 16,
-				.rate = 8000,
-				.dac_dap_data_comm_format =
-					dac_dap_data_format_dsp,
-			},
-		},
-		[4] = {
-			.dac_port = tegra_das_port_none,
-			.dap_port = tegra_das_port_none,
-			.codec_type = tegra_audio_codec_type_none,
-			.device_property = {
-				.num_channels = 0,
-				.bits_per_sample = 0,
-				.rate = 0,
-				.dac_dap_data_comm_format = 0,
-			},
-		},
-	},
-
-	.tegra_das_con_table = {
-		[0] = {
-			.con_id = tegra_das_port_con_id_hifi,
-			.num_entries = 2,
-			.con_line = { /*src*/            /*dst*/             /* src master */
-#ifdef ALC5623_IS_MASTER			
-				[0] = {tegra_das_port_i2s1, tegra_das_port_dap1, true}, 
-				[1] = {tegra_das_port_dap1, tegra_das_port_i2s1, false},
-#else
-				[0] = {tegra_das_port_i2s1, tegra_das_port_dap1, false}, 
-				[1] = {tegra_das_port_dap1, tegra_das_port_i2s1, true},
-#endif
-			},
-		},
-		[1] = {
-			.con_id = tegra_das_port_con_id_bt_codec,
-			.num_entries = 4,
-			.con_line = {
-				[0] = {tegra_das_port_i2s2, tegra_das_port_dap4, true}, /* src is master */
-				[1] = {tegra_das_port_dap4, tegra_das_port_i2s2, false},
-#ifdef ALC5623_IS_MASTER
-				[2] = {tegra_das_port_i2s1, tegra_das_port_dap1, true},
-				[3] = {tegra_das_port_dap1, tegra_das_port_i2s1, false},
-#else				
-				[2] = {tegra_das_port_i2s1, tegra_das_port_dap1, false},
-				[3] = {tegra_das_port_dap1, tegra_das_port_i2s1, true},
-#endif				
-			},
-		},
-		[2] = {
-			.con_id = tegra_das_port_con_id_voicecall_no_bt,
-			.num_entries = 4,
-			.con_line = {
-				[0] = {tegra_das_port_dap2, tegra_das_port_dap3, true},
-				[1] = {tegra_das_port_dap3, tegra_das_port_dap2, false},
-#ifdef ALC5623_IS_MASTER
-				[2] = {tegra_das_port_i2s1, tegra_das_port_dap1, true},
-				[3] = {tegra_das_port_dap1, tegra_das_port_i2s1, false},
-#else
-				[2] = {tegra_das_port_i2s1, tegra_das_port_dap1, false},
-				[3] = {tegra_das_port_dap1, tegra_das_port_i2s1, true},
-#endif
-			},
-		},
-	}
-};  
-
-static struct resource das_resource[] = {
-	[0] = {
-		.start	= TEGRA_APB_MISC_BASE,
-		.end	= TEGRA_APB_MISC_BASE + TEGRA_APB_MISC_SIZE - 1,
-		.flags	= IORESOURCE_MEM
-	}
-};
-
-static struct platform_device das_device = {
-	.name		= "tegra_das",
-	.id		= -1,
-	.resource	= das_resource,
-	.num_resources	= ARRAY_SIZE(das_resource),
-	.dev = {
-		.platform_data = &tegra_das_pdata,
-	}
-}; 
-#endif
-
 static struct tegra_audio_platform_data tegra_spdif_pdata = {
 	.dma_on			= true,  /* use dma by default */
 	.mask			= TEGRA_AUDIO_ENABLE_TX | TEGRA_AUDIO_ENABLE_RX,
@@ -285,7 +143,6 @@ static struct tegra_alc5623_platform_data adam_audio_pdata = {
         .gpio_spkr_en           = -2,
         .gpio_hp_det            = ADAM_HP_DETECT,
 	.gpio_int_mic_en 	= ADAM_INT_MIC_EN,
-#ifndef USE_ORG_DAS	
 	.hifi_codec_datafmt = SND_SOC_DAIFMT_I2S,	/* HiFi codec data format */
 #ifdef ALC5624_IS_MASTER
 	.hifi_codec_master  = true,			/* If Hifi codec is master */
@@ -294,8 +151,6 @@ static struct tegra_alc5623_platform_data adam_audio_pdata = {
 #endif
 	.bt_codec_datafmt   = SND_SOC_DAIFMT_DSP_A,	/* Bluetooth codec data format */
 	.bt_codec_master    = true,			/* If bt codec is master */
-#endif
-
 };
 
 static struct platform_device tegra_generic_codec = {
@@ -316,11 +171,7 @@ static struct platform_device *adam_i2s_devices[] __initdata = {
 	&tegra_i2s_device1,
 	&tegra_i2s_device2,
 	&tegra_spdif_device,
-#ifdef USE_ORG_DAS
-	&das_device,
-#else
 	&tegra_das_device,
-#endif
 	&spdif_dit_device,
 	&tegra_pcm_device,
 	&tegra_generic_codec,
@@ -346,4 +197,3 @@ int  __init adam_audio_register_devices(void)
 
         return platform_add_devices(adam_i2s_devices, ARRAY_SIZE(adam_i2s_devices));
 }
-
